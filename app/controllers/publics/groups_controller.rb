796f2ee9bbba197991291a67931951424650124d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-20 14:59:14 +0000

    コメント

--- app/controllers/publics/groups_controller.rb
+++ app/controllers/publics/groups_controller.rb
@@ -1,3 +1,11 @@
 class Publics::GroupsController < ApplicationController
-  
+
+  def new
+    @group =Group.new
+  end
+
+  def index
+    @book = Book.new
+    @groups = Group.all
+  end
 end
--- app/controllers/publics/rooms_controller.rb
+++ app/controllers/publics/rooms_controller.rb
@@ -1,2 +1,3 @@
 class Publics::RoomsController < ApplicationController
+  
 end
