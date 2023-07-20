4b3cde7eaa1dede306bf0df153b545fd48951a74
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-20 12:28:00 +0000

    作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_20_111719) do
+ActiveRecord::Schema.define(version: 2023_07_20_121726) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,13 @@ ActiveRecord::Schema.define(version: 2023_07_20_111719) do
     t.string "name"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
