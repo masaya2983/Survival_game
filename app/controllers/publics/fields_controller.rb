37d92c715e14e8264a5955feba5a5e6e59c14312
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-21 12:23:43 +0000

    コメント

--- app/controllers/publics/fields_controller.rb
+++ app/controllers/publics/fields_controller.rb
@@ -1,2 +1,56 @@
 class Publics::FieldsController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :ensure_correct_customer, only: [:edit, :update, :destroy]
+
+  def show
+    @field = Field.find(params[:id])
+    @field_comment = FieldComment.new
+  end
+
+  def index
+    @fields = Field.all.order(params[:sort])
+    @field = Field.new
+  end
+
+  def create
+    @field = Field.new(field_params)
+    @field.customer_id = current_customer.id
+    tag_list = params[:field][:tag_name].split(',')
+    if @field.save
+      @field.save_tags(tag_list)
+      redirect_to field_path(@field), notice: "You have created field successfully."
+    else
+      @fields = Field.all
+      render 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @field.update(field_params)
+      redirect_to field_path(@field), notice: "You have updated field successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @field.destroy
+    redirect_to fields_path
+  end
+
+  private
+
+  def field_params
+    params.require(:field).permit(:name, :body, :rate)
+  end
+
+  def ensure_correct_customer
+    @field = Field.find(params[:id])
+    unless @field.field == current_customer
+      redirect_to fields_path
+    end
+  end
 end
