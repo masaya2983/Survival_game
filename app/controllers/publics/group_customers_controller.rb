1882364d68c69e64220f66c971c6eb163dfbec5b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-21 09:17:55 +0000

    コメント

--- app/controllers/publics/group_customers_controller.rb
+++ app/controllers/publics/group_customers_controller.rb
@@ -1,3 +1,16 @@
 class Publics::GroupCustomersController < ApplicationController
+   before_action :authenticate_user!
+  
+  def create
+    group_customer = current_customer.group_customers.new(group_id: params[:group_id])
+    group_customer.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+    group_customer = current_customer.group_customers.find_by(group_id: params[:group_id])
+    group_customer.destroy
+    redirect_to request.referer
+  end
   
 end
