f90a859de2829211f5fa937082bf9747e454cc98
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-21 08:17:45 +0000

    コメント

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,23 @@
 class Tag < ApplicationRecord
+  has_many :field_tags, dependent: :destroy, foreign_key: 'tag_id'
+  has_many :fields, through: :fieldk_tags
+
+  scope :merge_books, -> (tags){ }
+
+  def self.search_books_for(content, method)
+
+    if method == 'perfect'
+      tags = Tag.where(name: content)
+    elsif method == 'forward'
+      tags = Tag.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      tags = Tag.where('name LIKE ?', '%' + content)
+    else
+      tags = Tag.where('name LIKE ?', '%' + content + '%')
+    end
+
+    return tags.inject(init = []) {|result, tag| result + tag.books}
+
+  end
+
 end
