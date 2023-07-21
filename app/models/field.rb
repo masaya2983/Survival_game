3c685a4c109e7ddd398041c6408af17850d63c13
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-21 07:28:46 +0000

    コメント

--- app/models/field.rb
+++ app/models/field.rb
@@ -15,13 +15,13 @@ class Field < ApplicationRecord
 
   def self.search_for(content, method)
     if method == 'perfect'
-      Book.where(title: content)
+      Field.where(title: content)
     elsif method == 'forward'
-      Book.where('title LIKE ?', content+'%')
+      Field.where('title LIKE ?', content+'%')
     elsif method == 'backward'
-      Book.where('title LIKE ?', '%'+content)
+      Field.where('title LIKE ?', '%'+content)
     else
-      Book.where('title LIKE ?', '%'+content+'%')
+      Field.where('title LIKE ?', '%'+content+'%')
     end
   end
 end
