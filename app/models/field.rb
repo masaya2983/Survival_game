005641a2db894392b27fcc0f3dcffde8807ee514
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-21 08:00:21 +0000

    コメント

--- app/models/field.rb
+++ app/models/field.rb
@@ -3,10 +3,10 @@ class Field < ApplicationRecord
   has_many :field_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :view_counts, dependent: :destroy
-
+  has_many :group_users, dependent: :destroy
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
 
-  validates :title,presence:true
+  validates :name,presence:true
   validates :body,presence:true,length:{maximum:200}
 
   def favorited_by?(user)
