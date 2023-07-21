7795f9047694b54ac4e24e1fa0c9e0c7b06b9d5d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-21 09:08:14 +0000

    コメント

--- app/controllers/publics/group_customers_controller.rb
+++ app/controllers/publics/group_customers_controller.rb
@@ -1,2 +1,3 @@
 class Publics::GroupCustomersController < ApplicationController
+  
 end
--- app/controllers/publics/groups_controller.rb
+++ app/controllers/publics/groups_controller.rb
@@ -8,4 +8,44 @@ class Publics::GroupsController < ApplicationController
     @field = Field.new
     @groups = Group.all
   end
+  
+  def show
+    @field = Field.new
+    @group = Group.find(params[:id])
+  end
+  
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_customer.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def ensure_correct_customer
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_customer.id
+      redirect_to groups_path
+    end
+  end
+  
 end
