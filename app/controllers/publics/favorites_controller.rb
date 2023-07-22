4b3e7f809dac0c832fb948795d95a01127e0afa2
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-22 03:50:21 +0000

    コメント

--- app/controllers/publics/favorites_controller.rb
+++ app/controllers/publics/favorites_controller.rb
@@ -1,2 +1,15 @@
 class Publics::FavoritesController < ApplicationController
+  def create
+    field = Field.find(params[:book_id])
+    @favorite = current_user.favorites.new(field_id: field.id)
+    @favorite.save
+    render 'replace_btn'
+  end
+
+  def destroy
+    field= Field.find(params[:field_id])
+    @favorite = current_user.favorites.find_by(field_id: field.id)
+    @favorite.destroy
+    render 'replace_btn'
+  end
 end
