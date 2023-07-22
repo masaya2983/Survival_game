f593a100414e146b02ad7984f14899e5b23e113e
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-22 08:14:28 +0000

    コメント

---
+++ app/controllers/admins/fields_controller.rb
@@ -0,0 +1,2 @@
+class Admins::FieldsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins,skip: [:registrations, :passwords], path: :admin, controllers:{
+   sessions: "admin/sessions"
+  }
+
+  devise_for :customers, skip: [:passwords], controllers:{
+   registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+   namespace :admin do
+   root :to => "homes#top"
+   resources :customers, only: [:index, :show, :update, :destroy]
+   resources :fieldss, only: [:index, :show, :update, :destroy, :new ]
+
+ end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
   root to: "home#top"
 end
