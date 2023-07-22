c2cbb580448653af24f5a808724c77f4eb7673d5
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-22 01:55:55 +0000

    コメント

--- app/controllers/publics/customerrooms_controller.rb
+++ app/controllers/publics/customerrooms_controller.rb
@@ -1,3 +1,42 @@
 class Publics::CustomerroomsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @customer = Customer.find(params[:id])
+    @fields = @customer.fields
+    @field = Field.new
+
+  end
+
+  def index
+    @customers = User.all
+    @field = Field.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_customer
+    @customer = Customer.find(params[:id])
+    unless @customer == current_customer
+      redirect_to customer_path(current_customer)
+    end
+  end
+
 end
